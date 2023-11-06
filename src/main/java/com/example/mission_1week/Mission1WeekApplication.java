03b06394f7110e67671a3e4a79576a334f37cb81
Aidennn-Lee <yw010804@gmail.com>
2023-11-06 10:15:45 +0900

    stage 1

--- src/main/java/com/example/mission_1week/Mission1WeekApplication.java
+++ src/main/java/com/example/mission_1week/Mission1WeekApplication.java
@@ -1,13 +1,21 @@
 package com.example.mission_1week;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class Mission1WeekApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(Mission1WeekApplication.class, args);
-    }
+        //SpringApplication.run(Mission1WeekApplication.class, args);
+        Scanner scanner = new Scanner(System.in);
+        String cmd;
 
+        System.out.println("== 명언 앱 ==");
+        while(true){
+            System.out.print("명령) ");
+            cmd = scanner.nextLine();
+            if(cmd.equals("종료")) break;
+        }
+    }
 }
