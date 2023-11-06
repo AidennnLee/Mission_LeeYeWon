4bfafc655ae87544090247d9bd97d3b9d2d4826f
Aidennn-Lee <yw010804@gmail.com>
2023-11-06 09:49:58 +0900

    start

---
+++ src/main/java/com/example/mission_1week/Mission1WeekApplication.java
@@ -0,0 +1,13 @@
+package com.example.mission_1week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mission1WeekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mission1WeekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mission_1week/Mission1WeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mission_1week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mission1WeekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
