765bb25fa58bbf7db104cb03d5e2f249acc455fe
Aidennn-Lee <yw010804@gmail.com>
2023-11-06 10:23:59 +0900

    stage 3

--- src/main/java/com/example/mission_1week/Mission1WeekApplication.java
+++ src/main/java/com/example/mission_1week/Mission1WeekApplication.java
@@ -22,6 +22,7 @@ public class Mission1WeekApplication {
                 content = scanner.nextLine();
                 System.out.print("작가 : ");
                 author = scanner.nextLine();
+                System.out.println("1번 명언이 등록되었습니다.");
             }
         }
     }
